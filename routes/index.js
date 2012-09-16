c01362b20577dc63bf101d13e2daf35a4556a46f
Doug Dymacek <achillies@achillies.com>
2012-09-16 01:14:02 -0500

    added table selector and some sample javascript

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,13 @@
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
-
 };
 
 /*
  * GET newGame page.
  */
-
 exports.newGame = function(req, res){
     res.render('newGame', { title: 'NewGame' });
-
 };
